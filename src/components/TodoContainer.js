e35bd6deb064b97c2016a4fff39901b343fe3c81
pockemon <hardikrana276@gmail.com>
2020-09-24 14:53:41 +0530

    react

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,76 @@
+import React from "react"
+import TodosList from "./TodosList";
+import Header from "./Header"
+import InputTodo from "./InputTodo"
+import { v4 as uuidv4 } from "uuid";
+import "../App.css"
+
+
+class TodoContainer extends React.Component {
+
+    state = {
+        todos: [{
+                id: 1,
+                title: "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "Deploy to live server",
+                completed: false
+            }
+        ]
+    };
+
+    handleChange = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        });
+    };
+
+    delTodo = id => {
+        this.setState({
+            todos: [
+                ...this.state.todos.filter(todo => {
+                    return todo.id !== id;
+                })
+            ]
+        });
+    };
+
+    addTodoItem = title => {
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false
+        };
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        });
+    };
+
+    render() {
+        return ( <
+            div className = "container" >
+            <Header / >
+            <InputTodo addTodoProps = { this.addTodoItem }/>
+            <TodosList todos = { this.state.todos }
+            handleChangeProps = { this.handleChange }
+            deleteTodoProps = { this.delTodo }
+            />
+            </div >
+        );
+    }
+}
+
+export default TodoContainer
---
+++ src/components/TodoItem.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+function TodoItem(props) {
+
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#d35e0f",
+    opacity: 0.4,
+    textDecoration: "line-through",
+   }
+
+    return (
+      <li className="todo-item">
+        <input
+          type="checkbox"
+          checked={props.todo.completed}
+          onChange={() => props.handleChangeProps(props.todo.id)}
+        />
+        <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+          Delete
+        </button>
+        <span style={props.todo.completed ? completedStyle : null}>
+          {props.todo.title}
+        </span>
+      </li>
+    )
+}
+
+export default TodoItem
