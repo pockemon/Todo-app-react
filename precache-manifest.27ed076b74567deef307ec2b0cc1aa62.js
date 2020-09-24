670262197663a7d8d64c8148e3e8406dd5d108df
pockemon <hardikrana276@gmail.com>
2020-09-24 14:58:11 +0530

    Updates

---
+++ precache-manifest.27ed076b74567deef307ec2b0cc1aa62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb6c8ce7c02590aebbe78e37d0fe048",
+    "url": "/Todo-app-react/index.html"
+  },
+  {
+    "revision": "33b2b01c00d0fbc31352",
+    "url": "/Todo-app-react/static/css/main.7225289e.chunk.css"
+  },
+  {
+    "revision": "156d86e658fe995faaf8",
+    "url": "/Todo-app-react/static/js/2.0d86fbe5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app-react/static/js/2.0d86fbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b2b01c00d0fbc31352",
+    "url": "/Todo-app-react/static/js/main.229d763d.chunk.js"
+  },
+  {
+    "revision": "d424e22439d7427d3564",
+    "url": "/Todo-app-react/static/js/runtime-main.a3479bdf.js"
+  }
+]);
